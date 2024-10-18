57b8ef6402bbe45310b06e3646383af01e9d93f3
Aaron Finke <aaron.finke@ess.eu>
2024-10-18 09:50:08 +0200

    initial commit

---
+++ nmx_workflow/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+from nmx_workflow import config  # noqa: F401
